9fd045e0297ad6509a069928e43a729d0c5d9586
Manuel Alcázar <manuzz3112@gmail.com>
2023-04-08 19:48:08 +0200

    mod ubicacion

--- src/app/pipes/date-compare.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateComparePipe } from './date-compare.pipe';
-
-describe('DateComparePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateComparePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
